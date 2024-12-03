8fc5496201c488efa3c74be51002da9f5968f06c
Czniartynsk <bruno.as72@gmail.com>
2024-12-03 18:11:38 -0300

    Aplicação iniciada - Configuração - json-server, webpack, webpack server, html, favicon, css, assets, babel e day.js

---
+++ src/main.js
@@ -0,0 +1,17 @@
+"use strict"
+
+// Importação da configuração do dayjs
+import "./libs/dayjs.js"
+
+// CSS
+import "./styles/global.css";
+import "./styles/app.css";
+import "./styles/button.css";
+import "./styles/agenda.css";
+import "./styles/list.css";
+import "./styles/inputs.css";
+import "./styles/footer.css";
+import "./styles/modal.css";
+
+// JS
+import "./services/schedule-new.js"
